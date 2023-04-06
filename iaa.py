fa3565c322e9b0fec4d3e0feeba2de5e3f8a6ff5
Jani Hietala <janimhietala@gmail.com>
2023-04-06 13:47:06 +0300

    Format with black

--- iaa.py
+++ iaa.py
@@ -3,9 +3,9 @@ import json
 
 from pyld import jsonld
 
-IAA_CONF_FILE = os.getenv('IAA_CONF_FILE', 'IAA.conf')
-OWNER_DID = os.getenv('OWNER_DID', 'did:self:1234oiuerhg98043n9hve')
-PROXY_PASS = os.getenv('PROXY_PASS', 'http://127.0.0.1:8000')
+IAA_CONF_FILE = os.getenv("IAA_CONF_FILE", "IAA.conf")
+OWNER_DID = os.getenv("OWNER_DID", "did:self:1234oiuerhg98043n9hve")
+PROXY_PASS = os.getenv("PROXY_PASS", "http://127.0.0.1:8000")
 
 LD_ACCESS_REQUIREMENTS = "https://twinschema.org/accessRequirements"
 LD_LOCATION = "http://www.w3.org/2003/01/geo/wgs84_pos#location"
@@ -16,115 +16,99 @@ iaa_conf = {
     "resources": {
         "/twins": {
             "authorization": {
-                "type":"jwt-vc-dpop",
+                "type": "jwt-vc-dpop",
                 "trusted_issuers": {
-                    OWNER_DID: {
-                        "issuer_key": OWNER_DID,
-                        "issuer_key_type": "did"
-                    }
+                    OWNER_DID: {"issuer_key": OWNER_DID, "issuer_key_type": "did"}
                 },
                 "filters": [
-                    ["$.vc.credentialSubject.capabilities.'https://iot-ngin.twinbase.org/twins'[*]", "READ"]
-                ]
+                    [
+                        "$.vc.credentialSubject.capabilities.'https://iot-ngin.twinbase.org/twins'[*]",
+                        "READ",
+                    ]
+                ],
             },
-            "proxy": {
-                "proxy_pass": PROXY_PASS
-            }
+            "proxy": {"proxy_pass": PROXY_PASS},
         },
-        "/docs": {
-            "proxy": {
-                "proxy_pass": PROXY_PASS
-            }
-        },
-        "/openapi.json": {
-            "proxy": {
-                "proxy_pass": PROXY_PASS
-            }
-        },
-        "/favicon.ico": {
-            "proxy": {
-                "proxy_pass": PROXY_PASS
-            }
-        }
+        "/docs": {"proxy": {"proxy_pass": PROXY_PASS}},
+        "/openapi.json": {"proxy": {"proxy_pass": PROXY_PASS}},
+        "/favicon.ico": {"proxy": {"proxy_pass": PROXY_PASS}},
     }
 }
+
 
 def get_conf_twin_template() -> dict:
     return {
         "authorization": {
             "type": "jwt-vc-dpop",
             "trusted_issuers": {
-                OWNER_DID: {
-                    "issuer_key": OWNER_DID,
-                    "issuer_key_type": "did"
-                }
+                OWNER_DID: {"issuer_key": OWNER_DID, "issuer_key_type": "did"}
             },
             "filters": [
                 [
                     "$.vc.credentialSubject.capabilities.'https://iot-ngin.twinbase.org/twins'[*]",
-                    "READ"
+                    "READ",
                 ]
-            ]
+            ],
         },
-        "proxy": {
-            "proxy_pass": PROXY_PASS
-        }
+        "proxy": {"proxy_pass": PROXY_PASS},
     }
+
 
 def configure(twin: dict, filters: list[str]) -> None:
     """Define twin conf"""
 
     try:
-        with open(IAA_CONF_FILE, 'r') as jsonfiler:
+        with open(IAA_CONF_FILE, "r") as jsonfiler:
             conf = json.load(jsonfiler)
     except FileNotFoundError:
-        print('IAA.conf not found, creating new from template.')
+        print("IAA.conf not found, creating new from template.")
         conf = iaa_conf
 
     conf_twin = get_conf_twin_template()
-    print('Conf template: ' + str(conf_twin))
+    print("Conf template: " + str(conf_twin))
 
     for filter_content in filters:
-        print('Creating filter: ' + filter_content)
+        print("Creating filter: " + filter_content)
         filter = [f"$.vc.credentialSubject.capabilities.'{filter_content}'[*]", "READ"]
-        conf_twin['authorization']['filters'].append(filter)
+        conf_twin["authorization"]["filters"].append(filter)
 
-    local_id = twin['dt-id'].split('/')[3]
-    conf['resources']['/twins/'+local_id] = conf_twin
+    local_id = twin["dt-id"].split("/")[3]
+    conf["resources"]["/twins/" + local_id] = conf_twin
 
-    with open(IAA_CONF_FILE, 'w', encoding='utf-8') as jsonfilew:
+    with open(IAA_CONF_FILE, "w", encoding="utf-8") as jsonfilew:
         json.dump(conf, jsonfilew, indent=4, ensure_ascii=False)
 
     print(conf_twin)
+
 
 def get_location_filters_for(document: dict) -> list[str]:
     filters: list[str] = []
     expanded_doc = jsonld.expand(document)
 
     if not len(expanded_doc) > 0:
-        print('Found no linked data.')
+        print("Found no linked data.")
         return filters
-    
+
     if LD_LOCATION not in expanded_doc[0]:
-        print(f'Found linked data, but not {LD_LOCATION}')
+        print(f"Found linked data, but not {LD_LOCATION}")
         return filters
 
     loc = expanded_doc[0][LD_LOCATION]
 
     if type(loc) is dict:
-        print('One location definitions found')
-        print('Value: ' + loc['@value'])
+        print("One location definitions found")
+        print("Value: " + loc["@value"])
         filters.append(f"{LD_NEIGHBOURHOOD} = {loc['@value']}")
 
     elif type(loc) is list:
-        print('Several location definitions found')
+        print("Several location definitions found")
         for location in loc:
             print(f"Type: {location['@type']} Value: {location['@value']}")
             # This @type & @value style was used at least in:
             # https://csiro-enviro-informatics.github.io/info-engineering/linked-data-api.html
-            if location['@type'] == LD_NEIGHBOURHOOD:
-                print('Found neighbourhood!')
+            if location["@type"] == LD_NEIGHBOURHOOD:
+                print("Found neighbourhood!")
                 filters.append(f"{LD_NEIGHBOURHOOD} = {location['@value']}")
-                print('Appended neighbourhood filter.')
+                print("Appended neighbourhood filter.")
 
     return filters
--- main.py
+++ main.py
@@ -25,16 +25,16 @@ baseurl = "https://juusoautiosalo.github.io/twinbase-smart-city"
 
 # GitHub setup https://stackoverflow.com/questions/44784828/gitpython-git-authentication-using-user-and-password
 username = "juusoautiosalo"
-password = os.getenv('GITHUB_TOKEN')
+password = os.getenv("GITHUB_TOKEN")
 if not password:
-    print('Twinbase API WARNING: GitHub token not set. SSH may still work.')
+    print("Twinbase API WARNING: GitHub token not set. SSH may still work.")
 remoteurl_https = f"https://{username}:{password}@github.com/{reponame}.git"
 remoteurl_ssh = "git@github.com:" + reponame + ".git"
 
 
 # Metadata for docs
 
-description= f"""
+description = f"""
 This page describes an HTTP API for: [{reponame}]({baseurl})
 
 You may send requests with the methods below.
@@ -43,19 +43,21 @@ You may send requests with the methods below.
 
 
 app = FastAPI(
-    title = "Twinbase API with SSI auth",
+    title="Twinbase API with SSI auth",
     description=description,
     version="0.0.1",
     docs_url=None,
     redoc_url=None,
 )
 
+
 class Twin(BaseModel):
-    dt_id: str = Field(alias='dt-id')
-    hosting_iri: str = Field(alias='hosting-iri')
+    dt_id: str = Field(alias="dt-id")
+    hosting_iri: str = Field(alias="hosting-iri")
     name: str
     description: str | None = None
     local_id: str
+
 
 # Helper functions
 
@@ -63,56 +65,64 @@ class Twin(BaseModel):
 
 # Favicon endpoints
 
-favicon_path = 'favicon.ico'
+favicon_path = "favicon.ico"
 
-@app.get('/favicon.ico', include_in_schema=False)
+
+@app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
     return FileResponse(favicon_path)
 
+
 @app.get("/docs", include_in_schema=False)
 def overridden_swagger():
-	return get_swagger_ui_html(openapi_url="/openapi.json", title=app.title, swagger_favicon_url=favicon_path)
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json", title=app.title, swagger_favicon_url=favicon_path
+    )
+
 
 @app.get("/redoc", include_in_schema=False)
 def overridden_redoc():
-	return get_redoc_html(openapi_url="/openapi.json", title=app.title, redoc_favicon_url=favicon_path)
+    return get_redoc_html(
+        openapi_url="/openapi.json", title=app.title, redoc_favicon_url=favicon_path
+    )
 
 
 # Twinbase API starts here
 
+
 @app.get("/")
 def read_root():
-    return {
-        "This is a ": "Twinbase API",
-        "See documentation in subfolder": "/docs"
-    }
+    return {"This is a ": "Twinbase API", "See documentation in subfolder": "/docs"}
+
 
 @app.get("/update")
 def update():
-    listurl = baseurl + "/" + '/index.json'
+    listurl = baseurl + "/" + "/index.json"
     r = requests.get(listurl)
-    twins = r.json()['twins']
+    twins = r.json()["twins"]
     for twin in twins:
-        print('\nChecking ' + twin['name'])
+        print("\nChecking " + twin["name"])
         # pprint.pprint(twin)
         try:
-            doc = dtweb.client.fetch_dt_doc(twin['dt-id'])
+            doc = dtweb.client.fetch_dt_doc(twin["dt-id"])
         except:
-            print('This twin is not working properly. Probably the DTID is not working.')
+            print(
+                "This twin is not working properly. Probably the DTID is not working."
+            )
             pass
         filters = iaa.get_location_filters_for(doc)
         iaa.configure(twin, filters)
-        
-    return {
-        "detail": "Update is not working yet :)"
-    }
+
+    return {"detail": "Update is not working yet :)"}
+
 
 @app.get("/twins")
 def read_twins():
-    listurl = baseurl + "/" + '/index.json'
+    listurl = baseurl + "/" + "/index.json"
     r = requests.get(listurl)
-    twins = r.json()['twins']
+    twins = r.json()["twins"]
     return twins
+
 
 @app.get("/twins/{local_id}")
 def read_twin(local_id: str):
@@ -121,17 +131,22 @@ def read_twin(local_id: str):
     if r.status_code == 200:
         twin = r.json()
     else:
-        return {
-            'detail': 'error',
-            'status-code': r.status_code
-            }
+        return {"detail": "error", "status-code": r.status_code}
     return twin
+
 
 @app.get("/twins/{local_id}/github")
 def read_twin_github(local_id: str):
-    jsonUrl = "https://raw.githubusercontent.com/" + reponame + "/main/docs/" + local_id + "/index.json"
+    jsonUrl = (
+        "https://raw.githubusercontent.com/"
+        + reponame
+        + "/main/docs/"
+        + local_id
+        + "/index.json"
+    )
     twin = requests.get(jsonUrl).json()
     return twin
+
 
 @app.get("/twins/{local_id}/global")
 def read_twin_global(local_id: str):
@@ -139,20 +154,18 @@ def read_twin_global(local_id: str):
     doc = dtweb.client.fetch_dt_doc(dt_id)
     return doc
 
+
 @app.patch("/twins/{local_id}")
 def update_twin(local_id: str, patch: dict):
-    jsonUrl = baseurl + '/' + local_id + '/index.json'
+    jsonUrl = baseurl + "/" + local_id + "/index.json"
     r = requests.get(jsonUrl)
     if r.status_code == 200:
         twin = r.json()
     else:
-        return {
-            'detail': 'twin not found',
-            'status-code': r.status_code
-            }
+        return {"detail": "twin not found", "status-code": r.status_code}
     twin.update(patch)
 
-    tempdir = 'temporary_directory_for_twinbase_api'
+    tempdir = "temporary_directory_for_twinbase_api"
     curdir = os.getcwd()
     parentdir = os.path.dirname(curdir)
     gitdir = os.path.join(parentdir, tempdir)
@@ -169,17 +182,16 @@ def update_twin(local_id: str, patch: dict):
     repo.config_writer().set_value("user", "name", "Juuso Autiosalo").release()
     repo.config_writer().set_value("user", "email", "juuso.autiosalo@iki.fi").release()
 
-
-    twindoc_filepath = gitdir + '/docs/' + local_id + '/index.json'
-    with open(twindoc_filepath, 'w') as jsonfilew:
+    twindoc_filepath = gitdir + "/docs/" + local_id + "/index.json"
+    with open(twindoc_filepath, "w") as jsonfilew:
         json.dump(twin, jsonfilew, indent=4)
 
-    with open(twindoc_filepath, 'r') as jsonfiler:
+    with open(twindoc_filepath, "r") as jsonfiler:
         print(json.load(jsonfiler))
 
     repo.index.add([twindoc_filepath])
-    
-    repo.index.commit("Update " + twin['name'])
+
+    repo.index.commit("Update " + twin["name"])
 
     origin = repo.remote(name="origin")
     origin.push()
@@ -187,12 +199,13 @@ def update_twin(local_id: str, patch: dict):
     shutil.rmtree(gitdir)
     return twin
 
+
 @app.post("/twins/")
 def create_twin(twin: Twin):
     twin.local_id = str(uuid.uuid4())
     twin.dt_id = "https://dtid.org/" + twin.local_id
     twin.hosting_iri = baseurl + "/" + twin.local_id
-    tempdir = 'temporary_directory_for_twinbase_api'
+    tempdir = "temporary_directory_for_twinbase_api"
     curdir = os.getcwd()
     parentdir = os.path.dirname(curdir)
     gitdir = os.path.join(parentdir, tempdir)
@@ -210,15 +223,15 @@ def create_twin(twin: Twin):
     repo.config_writer().set_value("user", "name", "twinbase-bot").release()
     repo.config_writer().set_value("user", "email", "bot@twinbase.org").release()
 
-    twindoc_filepath = gitdir + '/docs/' + twin.local_id + '/index.json'
-    os.mkdir(gitdir + '/docs/' + twin.local_id)
+    twindoc_filepath = gitdir + "/docs/" + twin.local_id + "/index.json"
+    os.mkdir(gitdir + "/docs/" + twin.local_id)
     twindict = dict(twin)
-    twindict['dt-id'] = twindict.pop('dt_id')
-    twindict['hosting-iri'] = twindict.pop('hosting_iri')
-    with open(twindoc_filepath, 'w+') as jsonfilew:
+    twindict["dt-id"] = twindict.pop("dt_id")
+    twindict["hosting-iri"] = twindict.pop("hosting_iri")
+    with open(twindoc_filepath, "w+") as jsonfilew:
         json.dump(twindict, jsonfilew, indent=4)
 
-    with open(twindoc_filepath, 'r') as jsonfiler:
+    with open(twindoc_filepath, "r") as jsonfiler:
         print(json.load(jsonfiler))
 
     repo.index.add([twindoc_filepath])
@@ -226,23 +239,20 @@ def create_twin(twin: Twin):
     origin = repo.remote(name="origin")
     origin.push()
 
-
     shutil.rmtree(gitdir)
     return twin
 
+
 @app.delete("/twins/{local_id}")
 def delete_twin(local_id: str):
-    jsonUrl = baseurl + '/' + local_id + '/index.json'
+    jsonUrl = baseurl + "/" + local_id + "/index.json"
     r = requests.get(jsonUrl)
     if r.status_code == 200:
         twin = r.json()
     else:
-        return {
-            'detail': 'twin not found',
-            'status-code': r.status_code
-            }
+        return {"detail": "twin not found", "status-code": r.status_code}
 
-    tempdir = 'temporary_directory_for_twinbase_api'
+    tempdir = "temporary_directory_for_twinbase_api"
     curdir = os.getcwd()
     parentdir = os.path.dirname(curdir)
     gitdir = os.path.join(parentdir, tempdir)
@@ -259,23 +269,21 @@ def delete_twin(local_id: str):
     repo.config_writer().set_value("user", "name", "twinbase-bot").release()
     repo.config_writer().set_value("user", "email", "bot@twinbase.org").release()
 
-
-    twindir = gitdir + '/docs/' + local_id
+    twindir = gitdir + "/docs/" + local_id
     try:
         shutil.rmtree(twindir)
     except FileNotFoundError:
-        return {'detail':'twin not found, probably recently deleted'}
+        return {"detail": "twin not found, probably recently deleted"}
 
     repo.git.add(all=True)
 
-    repo.index.commit("Delete " + twin['name'])
-
+    repo.index.commit("Delete " + twin["name"])
 
     origin = repo.remote(name="origin")
     origin.push()
 
     shutil.rmtree(gitdir)
-    return "Removed " + twin['name']
+    return "Removed " + twin["name"]
 
 
 # Update conf file at startup
